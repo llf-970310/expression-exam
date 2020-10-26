6040384a6c30567695df63e501ee7d430a85c681
Longfei Li <lf97310@gmail.com>
2020-10-26 20:22:05 +0800

    deploy: add Dockerfile and fix package import

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 import thriftpy2
 from thriftpy2.rpc import make_client
 
-user_thrift = thriftpy2.load("../expression-api/thrift_idl/user.thrift", module_name="user_thrift")
-user_client = make_client(user_thrift.UserService, '127.0.0.1', 9092)
+user_thrift = thriftpy2.load("https://raw.githubusercontent.com/llf-970310/expression-api/master/thrift_idl/user.thrift", module_name="user_thrift")
+user_client = make_client(user_thrift.UserService, '81.68.117.198', 9092)
